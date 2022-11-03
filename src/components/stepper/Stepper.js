448dffb304790c6c4bae7f1960d695714009d1b7
Ridha Watanabe <xsjark@protonmail.com>
2022-11-03 17:15:20 -0500

    improved ux on camera

--- src/components/stepper/Stepper.js
+++ src/components/stepper/Stepper.js
@@ -3,7 +3,7 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 function Stepper({step}) {
     return (
         <div class="col-10 col-md-8 align-self-center my-5">
-            <ProgressBar now={step/5*100} />
+            <ProgressBar now={step/5*100 + 1/5*100} />
         </div>
     );
 }
